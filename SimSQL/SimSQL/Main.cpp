dd680d8115abaf4bffbcb9316c4938c84fa4f105
lyh4967 <lyh4965@naver.com>
2018-05-09 16:24:36 +0900

    file input,output 180509

--- SimSQL/SimSQL/Main.cpp
+++ SimSQL/SimSQL/Main.cpp
@@ -1,28 +1,64 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <vector>
 #include"QueType.h"
 #include "Table.h"
-#define D_MAX_ARRAY_SIZE 100
 using namespace std;
 
-void SQLProcessor(string s);
+enum DML{INSERT,DELETE,UPDATE,SELECT};
+QueType<string> StringSplit(string strTarget, string strTok); //string.split
+void SQLProcessor(string s,Table& table);
 
+//테이블이름으로 output파일 생성하자
+ofstream output("data.bin", ios::out | ios::binary);
 int main(){
 
-	//ofstream output("data.bin", ios::out | ios::binary);
+	
 	//ifstream input("data.bin", ios::in | ios::binary);
+	ifstream commandInput("commandSQL.txt");
+	Table table("test",3,"이름","나이","학교");//테이블(테이블이름, 필드길이,필드명)
 	cout << "Hello customer, This is SimSQL" << endl;
 	bool flag = true;
 	string sql;
 	while (flag){
 		cout << ">";
-		getline(cin, sql);
-		SQLProcessor(sql);
-		flag = false;
+		getline(commandInput, sql);//SQL입력받기
+		cout << sql << endl;
+		SQLProcessor(sql,table);//SQL처리기 (sql,테이블)
+		if (sql == "q")
+			flag = false;
 	}
 	return 0;
+}
+void SQLProcessor(string s, Table& table){
+	/*vector<string> tokens;
+	istringstream iss(s);
+	copy(istream_iterator<string>(iss),
+	istream_iterator<string>(),
+	back_inserter<vector<string> >(tokens));*/
+	QueType<string> commandQ(StringSplit(s, " "));//공백을 기준으로 구문 분리
+	string command;
+	commandQ.Dequeue(command);
+	if (command == "INSERT"){//INSERT시에
+		commandQ.Dequeue(command);
+		QueType<string> subQ(StringSplit(command, ","));//넘길 값들 subQ큐에 넣기
+		table.Insert(subQ);
+	}
+	else if (command == "SELECT"){//SELECT시에
+		commandQ.Dequeue(command);
+		if (command == "*"){//*이라면 모든 tupl 출력
+			table.Select();
+		}
+		else{
+
+		}
+	}
+	else if (command == "COMMIT") {//COMMIT시에
+		table.Commit();
+	}
+	else if (command == "READ") {
+		table.Read();
+	}
 }
 QueType<string> StringSplit(string strTarget, string strTok)
 {
@@ -45,20 +81,4 @@ QueType<string> StringSplit(string strTarget, string strTok)
 	}
 	
 	return Que;
-}
-
-void SQLProcessor(string s){
-	/*vector<string> tokens;
-	istringstream iss(s);
-	copy(istream_iterator<string>(iss),
-		istream_iterator<string>(),
-		back_inserter<vector<string> >(tokens));*/
-	QueType<string> tempQ (StringSplit(s, " "));
-	while (!tempQ.IsEmpty()){
-		string tmp;
-		tempQ.Dequeue(tmp);
-		cout << tmp<<endl;
-	}
-
-	
 }
--- SimSQL/SimSQL/Table.h
+++ SimSQL/SimSQL/Table.h
@@ -3,36 +3,106 @@
 #include <fstream>
 using namespace std;
 struct Tupl{
-	string infos[5];
+	string infos[3];
 	Tupl* next;
 	Tupl* prev;
 };
 
 class Table {
 private:
-	int info;
 	Tupl* listData;
 	string tbl_name;
-	string field[5];
-	int length;
+	int fieldLen;
+	string fieldName[3];
+	int length;//데이터 갯수
+	ofstream output;
+	ifstream input;
 public:
-	Table(){
-		listData = NULL;
-		length = 0;
+	Table(string name, int len, string field0, string field1, string field2);//constructor
+	//(테이블이름, 필드길이, 필드명)
+	//현재는 테이블생성이 정적임
+
+	void Select() const;//print 함수
+	//pre: table는 초기화 되어있다,
+	//post: table에 있는 필드명과 tupl을 출력한다.
+	
+	void Insert(QueType<string>& sql);
+	//pre: sql은 초기화되어있다.
+	//post: tupl이 추가된다.
+
+	void Commit();
+	//pre: table는 초기화되어있다.
+	//post: out파일에 테이블정보가 쓰여진다.
+
+	void Read();
+	//pre: input는 초기화되어 있어야한다.
+	//post: 테이블의 이름의 바이너리파일을 불러와 정보를 읽는다
+};
+Table::Table(string name, int len, string field0, string field1, string field2){//(테이블이름, 필드길이,필드명)
+	listData = NULL;
+	length = 0;
+	tbl_name = name;
+	fieldLen = len;
+	fieldName[0] = field0;
+	fieldName[1] = field1;
+	fieldName[2] = field2;
+}
+
+void Table::Select() const{
+	Tupl* location = listData;
+	for (int i = 0; i < fieldLen; i++){//필드명 출력
+		cout << fieldName[i] << ' ';
 	}
-	void Print(){
-		std::cout << info;
+	cout << endl;
+
+	for (int i = 0; i < length; i++){//테이블의 크기만큼
+		for (int i = 0; i < fieldLen; i++){ //튜플 출력
+			cout << location->infos[i] << ' ';
+		}
+		cout << endl;
+		location = location->next;
 	}
-	void Insert(string sql){
+}
+
+void Table::Insert(QueType<string>& sql){
+	string value;
+	int cnt = 0;
+	Tupl* location = new Tupl;
+	while (!sql.IsEmpty()){//날아온 순서대로 value값을 tupl에 넣는다.
+		sql.Dequeue(value);
+		location->infos[cnt] = value;// tupl에 차례로 value 저장
+		cnt++;
+	}
+	location->next = listData;
+	listData = location;
+	length++;
+	cout << "SUCCESS!" << endl;
+
+}
+void Table::Commit(){
+	string outputPath = tbl_name + ".bin";
+	output.open(outputPath,ios::out|ios::binary);
+	Tupl* location = listData;
+	while (!location == NULL) {
+		output.write((char*)&location->infos, sizeof(location->infos));
+		location = location->next;
+	}
+	output.close();
+	cout << "SUCCESS!" << endl;
+}
+
+void Table::Read() {
+	string inputPath = tbl_name + ".bin";
+	input.open(inputPath,ios::in|ios::binary);
+	length = 0;
+	while (!input.eof()) {
 		Tupl* location = new Tupl;
-		location->infos[0] = sql;// 요소 저장
+		input.read((char*)& location->infos, sizeof(location->infos));
 		location->next = listData;
 		listData = location;
+		length++;
 	}
-	void commit(std::ofstream& out){
-		out.write((char*)&info,sizeof(int));
-	}
-	void read(std::ifstream& in){
-		in.read((char*)&info, sizeof(int));
-	}
-};
+	input.close();
+	cout << "SUCCESS! READ" << endl;
+
+}
